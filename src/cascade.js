526d6a6e55c99207efe3cf1b22d913068daa86aa
shreedhurgadevis141-eng <shreedhurga.devi.s.141@kalvium.community>
2026-08-04 11:07:14 +0530

    Add Prescription Cascade frontend page

---
+++ src/cascade.js
@@ -0,0 +1,6 @@
+{
+  patient_id: "p1",
+  clinical_note: "Patient started amlodipine and later developed ankle swelling.",
+  prescriptions: [{ drug_name: "Amlodipine", start_date: "2026-06-01" }],
+  symptoms: [{ symptom_name: "Ankle swelling", onset_date: "2026-06-21" }]
+}
